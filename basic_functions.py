3a167de0817db51b6b0417a1fe34726fb69d416e
Waqas Riaz <waqas@swift-logix.com>
2020-09-23 20:55:18 +0500

    added simple calc files

---
+++ basic_functions.py
@@ -0,0 +1,6 @@
+def add(x,y):
+    z= x+y
+    return z
+
+def subtract(x,y):
+    return x-y
---
+++ main.py
@@ -0,0 +1,11 @@
+import basic_functions as func
+
+a = 2
+b = 4
+c=0
+d=0
+c = func.add(a,b)
+d = func.subtract(a,b)
+
+print ("c =add(a,b) " , c)
+print ("d =subtract(a,b) " , d)
